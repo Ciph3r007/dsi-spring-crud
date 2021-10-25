5f982d485da654d2464f2b3aacea1c9c94427b18
Ciph3r007 <apu466@gmail.com>
2021-10-25 15:37:44 +0600

    Initial Commit

---
+++ src/test/java/com/dsitraining/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsitraining.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
