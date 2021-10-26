d816c11ca9b429ee68e6182b947fb351dd78eaa9
Ciph3r007 <apu466@gmail.com>
2021-10-26 13:58:21 +0600

    Add product details page

--- src/main/java/com/dsitraining/crud/MainController.java
+++ src/main/java/com/dsitraining/crud/MainController.java
@@ -4,9 +4,13 @@ import com.dsitraining.crud.product.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.websocket.server.PathParam;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -19,7 +23,24 @@ public class MainController {
     @GetMapping("")
     public String showIndex(Model model) {
         model.addAttribute("products", products);
-
         return "index";
+    }
+
+    @GetMapping("/product/{id}")
+    public String showProductDetails(@PathVariable int id, Model model) {
+        Product product = getProductById(id);
+        model.addAttribute("product", product);
+
+        return "product-details";
+    }
+
+    private Product getProductById(int id) {
+        for (var product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+
+        return null;
     }
 }
