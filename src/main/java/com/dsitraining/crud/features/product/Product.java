18d462cc231727eb0845e3ec3a6e39bb4d32d647
Ciph3r007 <apu466@gmail.com>
2021-11-14 12:10:34 +0600

    Reimplement features with oracle database

--- src/main/java/com/dsitraining/crud/features/product/Product.java
+++ src/main/java/com/dsitraining/crud/features/product/Product.java
@@ -1,46 +1,29 @@
 package com.dsitraining.crud.features.product;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "product")
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column
     private String name;
+
+    @Column
     private Integer price;
+
+    @Column
     private Integer quantity;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Product() {
-    }
 
     public Product(String name, Integer price, Integer quantity) {
         this.name = name;
--- src/main/java/com/dsitraining/crud/features/product/ProductService.java
+++ src/main/java/com/dsitraining/crud/features/product/ProductService.java
@@ -11,22 +11,29 @@ public class ProductService implements CrudService<Product> {
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+        add(new Product("Razer Deathadder Chroma", 59, 2));
+        add(new Product("RTX 3090", 1299, 5));
+        add(new Product("PlayStation 5", 999, 1));
     }
 
     public List<Product> getList() {
-        return productRepository.getProducts();
+        return productRepository.findAll();
     }
 
     public void add(Product product) {
-        productRepository.add(product);
+        productRepository.save(product);
     }
 
-    public void update(Product product, int id) {
-        productRepository.update(product, id);
+    public void update(Product productWithUpdate, int id) {
+        var product = productRepository.getById(id);
+        product.setName(productWithUpdate.getName());
+        product.setPrice(productWithUpdate.getPrice());
+        product.setQuantity(productWithUpdate.getQuantity());
+        productRepository.save(product);
     }
 
     public void delete(int id) {
-        productRepository.delete(id);
+        productRepository.deleteById(id);
     }
 
     public Product getById(int id) {
